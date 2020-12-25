e3cd1f6753d937f479fcadf5b5086dc48b999fcb
Ata Atasoy <h.ataatasoy@gmail.com>
2020-12-25 16:10:43 +0300

    wip.

--- web/src/components/FilePreview.jsx
+++ web/src/components/FilePreview.jsx
@@ -3,33 +3,11 @@ import { Carousel } from 'antd';
 
 
 export default function FilePreview({ previewImage, annotation }) {
-  const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
-  
-  function onChange(a, b, c) {
-    console.log(a, b, c);
-  }
-
-
   return (
     <div>
       <Carousel autoplay>
         <div>
-          <h3 style={contentStyle}>1</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>3</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>4</h3>
+          <img alt="" src={previewImage}/>
         </div>
       </Carousel>
     </div>
