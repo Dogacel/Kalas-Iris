f799ea5f2000f42b9f5fec374f65ff9654e563b9
Ata Atasoy <h.ataatasoy@gmail.com>
2020-12-29 12:22:57 +0300

    Remove the comments for redirection

--- api/flaskr/__init__.py
+++ api/flaskr/__init__.py
@@ -8,6 +8,7 @@ from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
 
+
 def create_app(test_config=None):
     # create and configure the app
     load_dotenv()
@@ -17,14 +18,16 @@ def create_app(test_config=None):
 
     DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-    
+
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
 
     # Create the MongoDB instances
-    fashionImages_mongo = PyMongo(app, uri=f'mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@cluster0.ee0zf.mongodb.net/fashion_images?retryWrites=true&w=majority')
-    userInformation_mongo = PyMongo(app, uri=f'mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@cluster0.ee0zf.mongodb.net/user_information?retryWrites=true&w=majority')
+    fashionImages_mongo = PyMongo(
+        app, uri=f'mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@cluster0.ee0zf.mongodb.net/fashion_images?retryWrites=true&w=majority')
+    userInformation_mongo = PyMongo(
+        app, uri=f'mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@cluster0.ee0zf.mongodb.net/user_information?retryWrites=true&w=majority')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -43,15 +46,15 @@ def create_app(test_config=None):
     def index():
         if 'username' in session:
             return 'You are logged in as ' + session['username']
-                
-        return render_template('index.html')
+
+        # return render_template('index.html')
 
     @app.route('/uploadProductImage', methods=(['POST']))
     def uploadProductImage():
         file = request.files.get('image')
-        fashion_image_collection = fashionImages_mongo.db.images    
+        fashion_image_collection = fashionImages_mongo.db.images
         fashionImages_mongo.save_file(file.filename, file)
-        fashion_image_collection.insert({'photo_name': file.filename})    
+        fashion_image_collection.insert({'photo_name': file.filename})
         # Return more meaningful data maybe
 
         return 'Done!'
@@ -59,37 +62,36 @@ def create_app(test_config=None):
     @app.route('/file/<filename>')
     def file(filename):
         return fashionImages_mongo.send_file(filename)
-    
-    @app.route('/signup', methods=(['POST', 'GET']))
+
+    @app.route('/signup', methods=(['POST']))
     def signup():
-        if request.method == 'POST':
-            # Receive the form info
-            username = request.form['username']
-            password = request.form['password']
-            email = request.form['email']
 
-            # Get the users collection
-            users = userInformation_mongo.db.users
+        # Receive the form info
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
 
-            # Check for duplicates
-            existing_user = users.find_one({'username': username})
-            existing_email = users.find_one({'email': email})
+        # Get the users collection
+        users = userInformation_mongo.db.users
 
-            if (existing_user is None):
-                if (existing_email is None):
-                    # encrypt the password
-                    pw_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-                    users.insert({'username': username, 'email': email, 'password': pw_hash})
-                    session['username'] = username
-                    
-                    #return redirect(url_for('index'))
-                    return 'Redirect to index'
-                else:
-                    return 'That email already exists!'
+        # Check for duplicates
+        existing_user = users.find_one({'username': username})
+        existing_email = users.find_one({'email': email})
+
+        if (existing_user is None):
+            if (existing_email is None):
+                # encrypt the password
+                pw_hash = bcrypt.generate_password_hash(
+                    password).decode('utf-8')
+                users.insert(
+                    {'username': username, 'email': email, 'password': pw_hash})
+                session['username'] = username
+
+                return redirect(url_for('index'))
             else:
-                return 'That username already exists!'
-        
-        return render_template('register.html')
+                return 'That email already exists!'
+        else:
+            return 'That username already exists!'
 
     @app.route('/login', methods=(['POST']))
     def login():
@@ -106,7 +108,7 @@ def create_app(test_config=None):
             if bcrypt.check_password_hash(login_user['password'], password):
                 session['username'] = username
 
-                #return redirect(url_for('index'))
+                return redirect(url_for('index'))
 
         return 'Username and password do not match'
 
