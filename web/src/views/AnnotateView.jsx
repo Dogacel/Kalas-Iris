fe6ead44b8d12b1dfac7ba0a961d9d409898252b
Doğaç Eldenk <dogacel@gmail.com>
2021-02-01 11:50:39 +0300

    Fix root css

--- web/src/views/AnnotateView.jsx
+++ web/src/views/AnnotateView.jsx
@@ -6,6 +6,7 @@ import FileUpload from "../components/FileUpload";
 export default function AnnotateView() {
   const dummyJSON = ["Upload an image to start the process."];
 
+  // TODO: Filter shown annotations by count or confidence
   const [previewJSON, setPreviewJSON] = useState(dummyJSON);
   const [previewImage, setPreviewImage] = useState(
     "https://i.stack.imgur.com/y9DpT.jpg"
