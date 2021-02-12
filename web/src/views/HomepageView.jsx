5fbe7cc0cc014769fd595bce0b5ec98a7c1abe40
Olcay Akman <39094056+olcayakman@users.noreply.github.com>
2021-02-12 23:07:29 +0300

    Update HomepageView.jsx

--- web/src/views/HomepageView.jsx
+++ web/src/views/HomepageView.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "antd";
 import { Image } from "antd";
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Divider, message } from "antd";
+import { message } from "antd";
 
 
 const contentStyle = {
