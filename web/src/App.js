e9f37a1b79bcedb96e528945f32a9782dd6ad970
Olcay Akman <39094056+olcayakman@users.noreply.github.com>
2021-02-12 23:53:45 +0300

    Fix homepage code (#55)
    * homepage ready for demo
    
    * made changes in the carousel view on homepage
    
    * added dropdown nav-item to header navigation
    
    * removed the onFinish hook from FashionAnnotateInfoView
    
    * Remove onFinish hook and unused imports
    
    * Revert "Remove onFinish hook and unused imports "
    
    This reverts commit 103fc4642cf7b981b6b50bb28b6bcea79909f10b.
    
    * Update HomepageView.jsx
    
    * Update HomepageView.jsx
    
    * Update App.js
    
    * Update HomepageView.jsx
    
    * Update App.js
    
    * Update App.js
    
    * Update App.js
    
    Co-authored-by: Ata Atasoy <h.ataatasoy@gmail.com>

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import { Divider, Layout, Menu, Switch } from "antd";
+import { Divider, Layout, Menu, Switch, Dropdown } from "antd";
+import { DownOutlined } from '@ant-design/icons';
 import { useEffect, useState } from "react";
 import { WarningOutlined, CheckOutlined } from "@ant-design/icons";
 import { Route, Link, useLocation } from "react-router-dom";
@@ -10,7 +11,10 @@ import "./css/app.css";
 import LoginView from "./views/LoginView";
 import RegistrationForm from "./views/SignupView";
 import HomepageView from "./views/HomepageView";
+import FashionAnnotationInfoView from "./views/FashionAnnotationInfoView";
+import MenuItem from "antd/lib/menu/MenuItem";
 import { UserProvider } from "./components/UserContext";
+
 
 const { Header, Content, Footer } = Layout;
 
@@ -18,7 +22,6 @@ function App() {
   const location = useLocation();
 
   const menuLinks = [
-    //{ to: "/homepage", text: "Kalas-Iris" },
     { to: "/annotate", text: "Image Annotation" },
     { to: "/integrations", text: "Integrations" },
     { to: "/login", text: "Login" },
@@ -49,101 +52,119 @@ function App() {
   }, []);
 
   const menuBarStyle = {
-    primaryColor: "#f7c873",
-    backgroundColor: "#f7c873",
-    //color: "#bd8e02",
     color: '#434343',
     textColor: '#434343',
-    "&:hover": {
-      background: "#434343"
-    }
+    backgroundColor: "#001529"
   };
 
-  // const [isShown, setIsShown] = useState(false);
+  const [isShown] = useState(false);
+
+  const dropdown1 = (
+    <Menu>
+      <Menu.Item>
+        <Link to="/annotation-info">
+          What is Fashion Annotation?
+        </Link>
+      </Menu.Item>
+      <MenuItem>
+        <Link to="/annotation-info">
+          How it works
+        </Link>
+      </MenuItem>
+    </Menu>
+  );
 
 
   return (
-    <UserProvider>
 
-      <Layout className="layout">
-        <Header style={menuBarStyle}>
-          <div className="logo" />
-          <Menu
-            style={menuBarStyle}
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={[location.pathname]}
-          >
-            <>
-              <Menu.Item style={menuBarStyle}>
-                <Link to={"/homepage"}>
-                  <img
-                    alt="homepage_logo"
-                    width={189}
-                    height={43}
-                    src={process.env.PUBLIC_URL + '/ki-homepage-logo.png'}
-                    style={menuBarStyle}
-                  />
-                </Link>
+    <UserProvider>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={[location.pathname]}
+        >
+          <>
+            <Menu.Item style={menuBarStyle}>
+              <Link to={"/homepage"}>
+                <img
+                  width={189}
+                  height={43}
+                  src={process.env.PUBLIC_URL + '/ki-logo-white.png'}
+                  alt="logo"
+                  style={{ width: 190, height: 55 }}
+                />
+              </Link>
+            </Menu.Item>
+          </>
+          <>
+            {menuLinks.map(e => (
+              <Menu.Item
+                key={e.to}
+              >
+                <Link to={e.to} id="Link" key="Link">{e.text}{isShown}</Link>
               </Menu.Item>
-            </>
-            <>
-              {menuLinks.map(e => (
-                <Menu.Item
-                  key={e.to}
-                  id="hover-item"
-                  style={menuBarStyle}
-                >
-                  <Link to={e.to} id="Link" key="Link" style={{ color: "#434343" }}>{e.text}{false}</Link>
-                </Menu.Item>
-              ))}
-            </>
-            <Menu.Item id="cloud-menu-item" key="cloud-menu-item" disabled={true}>
-              Server Status{" "}
-              <Switch
-                loading={loading}
-                style={{ margin: "0px 4px 4px 4px" }}
-                onChange={() => {
-                  setLoading(true);
-                  setStall(true);
-                  isServerUp().then(r => {
-                    if (!r) upServer().then(setStall(false));
-                    else downServer().then(setStall(false));
-                  });
-                }}
-                checked={switchState}
-                checkedChildren={<CheckOutlined />}
-                unCheckedChildren={<WarningOutlined />}
-                defaultChecked
-              />
+            ))}
+          </>
+          <>
+          <Dropdown overlay={dropdown1} placement="bottomCenter">
+              <a className="ant-dropdown-link" onClick={e => e.preventDefault()} href="/homepage">
+                Fashion Annotation <DownOutlined/>
+              </a>
+          </Dropdown>
+          </>
+          <Menu.Item id="cloud-menu-item" key="cloud-menu-item" disabled={true}>
+            Server Status{" "}
+            <Switch
+              loading={loading}
+              style={{ margin: "0px 4px 4px 4px" }}
+              onChange={() => {
+                setLoading(true);
+                setStall(true);
+                isServerUp().then(r => {
+                  if (!r) upServer().then(setStall(false));
+                  else downServer().then(setStall(false));
+                });
+              }}
+              checked={switchState}
+              checkedChildren={<CheckOutlined />}
+              unCheckedChildren={<WarningOutlined />}
+              defaultChecked
+            />
             (Server shuts down every hour at xx:00)
           </Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: "25px 50px" }}>
-          <Route path="/annotate">
-            <AnnotateView />
-          </Route>
-          <Route path="/integrations">
-            <IntegrationsView />
-          </Route>
-          <Route path="/login">
-            <LoginView />
-          </Route>
-          <Route path="/signup">
-            <RegistrationForm />
-          </Route>
-          <Route path="/homepage">
-            <HomepageView />
-          </Route>
-          <Route path="/">
-            <Redirect to="/homepage" />
-          </Route>
-          <Route path="/"></Route>
-        </Content>
-        <Divider />
-        <Footer style={{ textAlign: "center" }}>
-          Kalas Iris ©2021 Created by Kalas Iris Team
+        </Menu>
+      </Header>
+      <Content style={{ padding: "25px 50px" }}>
+        <Route path="/annotate">
+          <AnnotateView />
+        </Route>
+        <Route path="/integrations">
+          <IntegrationsView />
+        </Route>
+        <Route path="/login">
+          <LoginView />
+        </Route>
+        <Route path="/signup">
+          <RegistrationForm />
+        </Route>
+        <Route path="/annotation-info">
+          <FashionAnnotationInfoView />
+        </Route>
+        <Route path="/homepage">
+          <HomepageView />
+        </Route>
+        <Route path="/">
+          <Redirect to="/homepage" />
+        </Route>
+        <Route path="/"></Route>
+      </Content>
+      <Divider />
+      <Footer style={{ textAlign: "center" }}>
+        Kalas Iris ©2021 Created by Kalas Iris Team
+
       </Footer>
       </Layout>
     </UserProvider>
---
+++ web/src/views/FashionAnnotationInfoView.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function FashionAnnotationInfoView() {
+    
+    return (
+
+        <>
+            <h1>info about fashion annotation</h1>
+        </>
+
+    );
+
+}
