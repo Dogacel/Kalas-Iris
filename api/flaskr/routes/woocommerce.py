642d18e8146541082fa837ccc7548d1778848311
Ata Atasoy <h.ataatasoy@gmail.com>
2021-02-13 20:08:53 +0300

    Implement Product API functions
    retrieveProduct, createProduct, updateProduct, listAllProducts,  listAllCategories

--- api/flaskr/routes/woocommerce.py
+++ api/flaskr/routes/woocommerce.py
@@ -4,20 +4,24 @@ from ..wcapi import woocommerce
 
 woocommerce_route = Blueprint('woocommerce_route', __name__)
 
-@woocommerce_route.route("/createProduct", methods='POST')
+@woocommerce_route.route("/createProduct", methods=['POST'])
 def createProduct():
     product_data = request.files.get('product_data')
     woocommerce.wcapi.post("products", product_data)
 
-@woocommerce_route("/retrieveProduct/<id>", methods='GET')
+@woocommerce_route.route("/retrieveProduct/<id>", methods=['GET'])
 def retrieveProduct(id):
-    woocommerce.wcapi.get(f'products/{id}')
+    return jsonify(woocommerce.wcapi.get(f'products/{id}').json())
 
-@woocommerce_route("/updateProduct/<id>", methods='POST')
+@woocommerce_route.route("/updateProduct/<id>", methods=['POST'])
 def updateProduct(id):
     product_data = request.files.get('product_data')
     woocommerce.wcapi.put(f'products/{id}', product_data)
 
-@woocommerce_route("/listAllProducts", methods='GET')
+@woocommerce_route.route("/listAllProducts", methods=['GET'])
 def listAllProducts():
-    woocommerce.wcapi.get("products")
+    return jsonify(woocommerce.wcapi.get("products").json())
+
+@woocommerce_route.route("/listAllCategories", methods=['GET'])
+def listAllCategories():
+    return jsonify(woocommerce.wcapi.get("products/categories").json())
