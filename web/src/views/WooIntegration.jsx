ef7f30a34164c8bdd07c35404a0a7c23c13761a3
Ata <Atasoy>
2021-02-18 22:42:51 +0300

    Update WooIntegration component

--- web/src/views/WooIntegration.jsx
+++ web/src/views/WooIntegration.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Form, Input, Button } from 'antd';
 
 export default function WooIntegration() {
-    return(
-        <div>WooIntegration</div>
+
+    const onFinish = (values) => {
+        console.log('Received values: ', values)
+        // Send request to backend
+    }
+
+    return (
+        <div className="WooCommerce">
+            <Form 
+                onFinish={onFinish}
+                initialValues={{
+                    remember: true,
+                  }}>
+                <Form.Item label="consumer-key">
+                    <Input type="text" placeholder="Consumer Key" />
+                </Form.Item>
+                <Form.Item label="consumer-secret">
+                    <Input type="text" placeholder="Consumer Secret" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
     )
 }
