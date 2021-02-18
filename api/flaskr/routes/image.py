d04e6927ba72a6f6c5c7a3fe3cc40474f3e7bc9d
Ata Atasoy <h.ataatasoy@gmail.com>
2021-02-18 21:32:11 +0300

    change annotateImage path. WIP.

--- api/flaskr/routes/image.py
+++ api/flaskr/routes/image.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, url_for, redirect, session, current_app, jsonify
 from flask.wrappers import Response
+from base64 import b64encode
 from ..db import mongo
+import requests
 
 image_route = Blueprint('image_route', __name__)
-
+mmfashionAPIAddress = "http://34.107.125.106"
 
 @image_route.route('/uploadProductImage', methods=(['POST']))
 def uploadProductImage():
@@ -19,3 +21,10 @@ def uploadProductImage():
 @image_route.route('/file/<filename>')
 def file(filename):
     return mongo.fashionImages.send_file(filename)
+
+
+@image_route.route('/annotateImage', methods=['POST'])
+def annotate():
+    image = request.files.get('image')
+    dictToSend = {'image': image}
+    return Response(requests.post(f'{mmfashionAPIAddress}/annotate', data=dictToSend))
