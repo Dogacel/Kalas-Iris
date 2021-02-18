f3157199d869cbb408cb49b8b6d2c3873edd3398
Ata Atasoy <40474912+AtaAtasoy@users.noreply.github.com>
2021-02-18 19:21:18 +0300

    WooCommerce Integration Implementation (#69)
    * initialize woocommerce API
    
    * initialize woocommerce API
    
    * Register woocommerce blueprint
    
    * Implement Product API functions
    
    retrieveProduct, createProduct, updateProduct, listAllProducts,  listAllCategories
    
    * Implement Product Categories and Tags API
    
    crateProductCategory, retrieveProductCategory, updateProductCategory, createProductTag, retrieveProductTag, updateProductTag, listAllProductTags
    
    * implement WebHook for Product created and bug fixes

---
+++ api/flaskr/wcapi.py
@@ -0,0 +1,12 @@
+from woocommerce import API
+import os
+
+
+class woocommerce:
+    wcapi = API(
+        url="https://wooatasoy.wpcomstaging.com",
+        consumer_key = os.getenv("WOO_CONSUMER_KEY"),
+        consumer_secret = os.getenv("WOO_CONSUMER_SECRET"),
+        wp_api = True,
+        version = "wc/v3"
+    )
