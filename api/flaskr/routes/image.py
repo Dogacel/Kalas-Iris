503794a7ab96e9488077c048596fe081fa9948ae
Doğaç Eldenk <dogacel@gmail.com>
2021-04-14 02:05:48 +0300

    Log annotations to db

--- api/flaskr/routes/image.py
+++ api/flaskr/routes/image.py
@@ -1,4 +1,6 @@
 import base64
+import random
+import string
 from flask import Blueprint, request, url_for, redirect, session, current_app, jsonify
 from flask.wrappers import Response
 from base64 import b64encode
@@ -24,11 +26,28 @@ def uploadProductImage():
 def file(filename):
     return mongo.fashionImages.send_file(filename)
 
+@image_route.route('/history')
+def history():
+    annotation_collection = mongo.fashionImages.db.annotations
+    return jsonify(dumps(list(annotation_collection.find({}))))
+
 
 @image_route.route('/annotateImage', methods=['POST'])
 def annotate():
     image = request.files.get('image')
     dictToSend = {'image': image.read()}
+    image.seek(0)
 
-    return jsonify(requests.post(f'{mmfashionAPIAddress}/annotate',
-                                 files=dictToSend).json())
+    response = 'a' # requests.post(f'{mmfashionAPIAddress}/annotate',
+                    #             files=dictToSend).json()
+
+    name = ''.join(random.choice(string.ascii_letters) for i in range(16))+image.filename   
+
+    fashion_image_collection = mongo.fashionImages.db.images
+    result = mongo.fashionImages.save_file(name, image)
+    print(result)
+
+    annotation_collection = mongo.fashionImages.db.annotations
+    annotation_collection.insert({'file_name': name, 'annotation': response})
+
+    return jsonify(response)
